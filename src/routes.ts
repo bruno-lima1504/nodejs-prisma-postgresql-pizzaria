94f7270120eff844319376cfb131263125089732
Bruno Lima <bruno.lima1504@gmail.com>
2023-11-13 22:08:07 -0300

    add: products: controller, service

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import  multer  from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUSerController } from './controllers/user/AuthUserController';
@@ -7,9 +8,16 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryControler } from './controllers/category/ListCategoryController';
 
+import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 // -- ROTAS USER --
 router.post('/users', new CreateUserController().handle );
@@ -24,6 +32,10 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 router.get('/category', isAuthenticated, new ListCategoryControler().handle);
 
+// -- ROTAS PRODUCT --
 
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+
+router.get('/product/category', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
